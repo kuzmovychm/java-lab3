1561fb651ce7d138eca30b417c9bc5e70f2038f8
Max Kuzmovych <kuzmovychm@gmail.com>
2019-02-18 09:08:07 +0200

    final version before passing

--- src/managers/GameRoomManager.java
+++ src/managers/GameRoomManager.java
@@ -36,28 +36,11 @@ public class GameRoomManager implements IGameRoomManager {
         }
     }
 
-    @Override
     public void sortToysByPrice(List<Toy> toys) {
-        Collections.sort(toys, new Comparator<Toy>() {
-            @Override
-            public int compare(Toy o1, Toy o2) {
-                return (int) (o1.getPrice() - o2.getPrice());
-            }
-        });
-
         toys.sort((Toy o1, Toy o2) -> (int) (o1.getPrice() - o2.getPrice()));
     }
 
-    @Override
     public void sortToysByType(List<Toy> toys) {
-        Collections.sort(toys, new Comparator<Toy>() {
-            @Override
-            public int compare(Toy o1, Toy o2) {
-                return o1.getToyType().compareTo(o2.getToyType());
-            }
-        });
-
-        //lambda
-        toys.sort((Toy o1, Toy o2)->o1.getToyType().compareTo(o2.getToyType()));
+        toys.sort((Toy o1, Toy o2)->o1.getClass().toString().compareTo(o2.getClass().toString()));
     }
 }
