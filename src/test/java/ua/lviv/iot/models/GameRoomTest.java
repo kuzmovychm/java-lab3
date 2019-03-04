b51e70776f3aeaef3da3b94790e0928e48419eba
Max Kuzmovych <kuzmovychm@gmail.com>
2019-03-04 11:44:55 +0200

    46% of tested code

---
+++ src/test/java/ua/lviv/iot/models/GameRoomTest.java
@@ -0,0 +1,34 @@
+package ua.lviv.iot.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameRoomTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void calculateMaximalNumberOfChildren() {
+    }
+
+    @Test
+    void calculateFreePlaces() {
+    }
+
+    @Test
+    void addChild() {
+    }
+
+    @Test
+    void removeChild() {
+    }
+
+    @Test
+    void addToy() {
+    }
+}
